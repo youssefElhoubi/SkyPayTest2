b14180b2ecebdfa7a7b49cfea0f6d4403656509e
youssefElhoubi <elhoubiyoussef@gmail.com>
2025-12-16 23:40:36 +0100

    created a booking entity

---
+++ src/main/java/com/entities/Booking.java
@@ -0,0 +1,20 @@
+package com.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Booking {
+    private  int bookingId;
+    private  User user;
+    private  Room roomNumber;
+    private  LocalDate checkIn;
+    private  LocalDate checkOut;
+    private  int bookedPricePerNight;
+    private  int totalCost;
+}
